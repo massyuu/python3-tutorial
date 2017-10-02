4bed0d9bb9c404a05a4da97b33622ce007ab3082
massyuu <massyuu.dear85y@gmail.com>
2017-10-02 22:57:54 +0900

    initial commit

---
+++ template.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# import
+from XXXXX import xxxxx
+
+# 定数宣言
+CONST_VALUE = 1
+
+def main():
+    # メイン処理はここに書くよ
+    print("this function is main")
+
+    functionA()
+
+def functionA():
+    # ここはサブの処理とか
+    print("this function is A")
+
+if __name__ == "__main__":
+    # ここはエントリーポイントのみ
+    main()
